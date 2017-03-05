1c6908141e6b41962704de94da10352da0b78b08
solominh <hoangminhftu2@gmail.com>
2017-03-05 22:53:41 +0700

    Add lingoes example

---
+++ examples/lingoes_chrome_3.0/background.js
@@ -0,0 +1,95 @@
+﻿var lingoes_port = null;
+var lingoes_msg_id = 0;
+
+function onLingoesMessage(msg) {
+  //console.log("Received:" + msg.cmd);
+  if(msg.cmd == "exit")
+  {
+    //console.log("on Exit");
+    closeLingoesPort();
+  }
+  else if(msg.cmd == "ping")
+  {
+  	lingoes_msg_id++;
+  	var out = {
+    	"cmd": "on_ping",
+    	"msg_id": lingoes_msg_id,
+    	"on_msg_id": msg.msg_id
+    }	
+    lingoes_port.postMessage(out);
+    //console.log("Send: " + JSON.stringify(out));		    
+  }
+  else if(msg.cmd == "text_capture")
+  {
+  	// 请求取词
+  	lingoes_msg_id++;
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		  chrome.tabs.sendMessage(tabs[0].id, {'cmd': 'text_capture', 'max_length': msg.max_length}, function(response) {
+		    var out = {
+		    	"cmd": "on_text_capture",
+		    	"msg_id": lingoes_msg_id,
+		    	"on_msg_id": msg.msg_id, 
+		    	"data": response 
+		    }
+
+		    lingoes_port.postMessage(out);
+		  	//console.log("Send: " + JSON.stringify(out));		    
+		  });
+		});
+  }
+  else if(msg.cmd == "text_select")
+  {
+  	// 请求划词
+  	lingoes_msg_id++;
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		  chrome.tabs.sendMessage(tabs[0].id, {'cmd': 'text_select', 'max_length': msg.max_length}, function(response) {
+		    var out = {
+		    	"cmd": "on_text_select",
+		    	"msg_id": lingoes_msg_id,
+		    	"on_msg_id": msg.msg_id, 
+		    	"data": response 
+		    };
+
+		    lingoes_port.postMessage(out);
+		    //console.log("Send: " + JSON.stringify(out));		    
+		  });
+		});
+  }
+}
+
+function onLingoesDisconnect() {
+  //console.log("failed to connect: " + chrome.runtime.lastError.message);
+  closeLingoesPort();
+}
+
+function closeLingoesPort() {
+	if(lingoes_port)
+	{
+	  lingoes_port.onMessage.removeListener(onLingoesMessage);
+	  lingoes_port.onDisconnect.removeListener(onLingoesDisconnect);
+	  lingoes_port.disconnect();
+	  lingoes_port = null;
+	}
+}
+
+function connectLingoesPort() {
+	if(lingoes_port == null)
+	{
+		// Connect Lingoes Native Messaging Server
+		lingoes_port = chrome.runtime.connectNative('com.lingoes.translator');
+		if(lingoes_port)
+		{
+			lingoes_port.onMessage.addListener(onLingoesMessage);
+			lingoes_port.onDisconnect.addListener(onLingoesDisconnect);
+			//console.log("connect to lingoes native message...")
+		}
+	}
+	
+	// Call connectLingoesPort() after 15 seconds, check this connection is valid
+	setTimeout(arguments.callee, 15000);
+}
+
+// Connect to Lingoes Native Messaging Server
+connectLingoesPort();
+
+//lingoes_port.postMessage({cmd: "ping"});
---
+++ examples/lingoes_chrome_3.0/lingoes_chrome.js
@@ -0,0 +1,104 @@
+﻿var lingoes_client_x = 0;
+var lingoes_client_y = 0;
+
+function lingoes_update_pos(e) {
+  lingoes_client_x = e.clientX;
+  lingoes_client_y = e.clientY;
+  //console.log("x=%d, y=%d\n", lingoes_client_x, lingoes_client_y);
+}
+
+function lingoes_get_capture_text(x, y) {
+  //console.log("x=%d, y=%d\n", lingoes_plugin_x, lingoes_plugin_y);
+  //var x = lingoes_plugin_x;
+  //var y = lingoes_plugin_y;
+  var a = document.caretRangeFromPoint(x, y);
+
+  if (a) {
+    var so = a.startOffset;
+    var eo = a.endOffset;
+    var g = a.cloneRange();
+    var maxchar = 100;
+    var pos = so;
+
+    // Prevent invalid at TEXTAREA,INPUT,SELECT
+    if (!a.startContainer.data)
+      return "";
+
+    //console.log(a.startContainer.data);
+    if (so <= 0 || eo >= a.endContainer.data.length)
+      return "";
+
+    var n1 = 0;
+    if (a.startContainer.data) {
+      for (; so > 0 && n1 < maxchar;) {
+        so--;
+        n1++;
+      }
+      g.setStart(a.startContainer, so);
+    }
+
+    pos -= (so + 1);
+    //console.log("pos=%d, n=%d, d=%n\n", pos, n1, d);
+
+    var n2 = 0;
+    if (a.endContainer.data) {
+      for (; eo < a.endContainer.data.length && n2 < maxchar;) {
+        eo++;
+        n2++;
+      }
+      g.setEnd(a.endContainer, eo);
+    }
+
+    if (n1 > 0 || n2 > 0) {
+      var str = g.toString();
+      if (str.length >= 0) {
+        //console.log("pos=%d, str=%s\n", pos, str);
+        return pos + ":" + str;
+      }
+    }
+  }
+
+  return "";
+}
+
+function lingoes_get_select_text() {
+  var str = String(window.getSelection());
+  if (str) {
+    str = str.replace(/^\s*/, "").replace(/\s*$/, "");
+
+    if (str != "")
+      return "0:" + str;
+  }
+
+  return "";
+}
+
+// Listener message from background.js
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.cmd == "text_capture") {
+    // On Capture Text from background.js
+    var text = lingoes_get_capture_text(lingoes_client_x, lingoes_client_y, message.max_length);
+    if (text != "") {
+      //console.log('CAPTURE: ' + text);
+      sendResponse(text);
+    }
+  }
+  else if (message.cmd == "text_select") {
+    // On Select Text from background.js
+    var text = lingoes_get_select_text(message.max_length);
+    if (text != "") {
+      //console.log('SELECT: ' + text);
+      sendResponse(text);
+    }
+  }
+  else {
+    //console.log("on message: " + message.cmd);
+  }
+
+});
+
+
+// "content_scripts": [ {"run_at": "document_start" } ]
+// In the case of "document_start", the files are injected after any files from css, 
+// but before any other DOM is constructed or any other script is run. 
+document.addEventListener("mousemove", lingoes_update_pos, false);
